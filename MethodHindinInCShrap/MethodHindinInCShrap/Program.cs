697d04de381b1fc9d4473eb2064c1db2d998f86e
Nakib <nakkicse@gmail.com>
2023-05-30 11:40:21 +0600

    Practice method hiding in c sharp

---
+++ MethodHindinInCShrap/MethodHindinInCShrap/Program.cs
@@ -0,0 +1,42 @@
+﻿public class Employee
+{
+    public string FirstName;
+    public string LastName;
+
+    public void PrintFullName()
+    {
+        Console.WriteLine(FirstName + " " + LastName);
+    }
+}
+
+public class FullTimeEmployee : Employee
+{
+
+}
+
+public class PartTimeEmployee : Employee
+{
+    public new void PrintFullName()
+    {
+        base.PrintFullName();
+        Console.WriteLine(FirstName + " " + LastName + " " + "Contractor");
+    }
+
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        FullTimeEmployee FTE = new FullTimeEmployee();
+        FTE.FirstName = "FullTime";
+        FTE.LastName = "Employee";
+        FTE.PrintFullName();
+
+        PartTimeEmployee PTE = new PartTimeEmployee();
+        PTE.FirstName = "PartTime";
+        PTE.LastName = "Employee";
+        PTE.PrintFullName();
+
+        ((Employee)FTE).PrintFullName();
+    }
+}
