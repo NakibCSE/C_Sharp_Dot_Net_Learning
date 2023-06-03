0fc99dc7d117478c5ec4f606e805fe191836003a
Nakib <nakkicse@gmail.com>
2023-06-03 11:41:06 +0600

    Method overloading practice

---
+++ MethodOverloading/MethodOverloading/Program.cs
@@ -0,0 +1,16 @@
+﻿
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        
+
+    }
+
+    public static void Add(int FN, int LN) { Console.WriteLine($"The some of the given number is = {FN + LN}"); }
+    public static void Add(string FN, int LN) { Console.WriteLine($"{FN} {LN}"); }
+    public static void Add(int FN,  double LN) { Console.WriteLine($"{FN + LN}"); }
+    public static int Add(int Number1, int Number2, int number3) { return Number1 + Number2 + number3; }
+    public static int Add(int number1, int number2, out int number3) { return number3 = number1 + number2;}
+
+}
