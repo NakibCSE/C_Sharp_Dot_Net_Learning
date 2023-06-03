f723eb8a5f80c9c535287f6d9bb1504fb1e31b11
Nakib <nakkicse@gmail.com>
2023-06-03 10:07:33 +0600

    Practice method overriding vs method hiding

---
+++ MethodOverridingVsMethodHiding/MethodOverridingVsMethodHiding/Program.cs
@@ -0,0 +1,21 @@
+﻿public class BaseClass
+{
+    public virtual void Print()
+    {
+        Console.WriteLine("I am a base class Print Method");
+    }
+}
+
+public class DeriverdClass : BaseClass
+{
+    public new void Print() { Console.WriteLine("I am a Derived Class Print Method"); }
+}
+
+public class Progam
+{
+    public static void Main(string[] args)
+    {
+        BaseClass b = new DeriverdClass();
+        b.Print();
+    }
+}
